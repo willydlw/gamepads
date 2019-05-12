2f5d86087174686a77d39dafce824391523cabe7
willydlw <willydlw@comcast.net>
2019-05-12 09:28:24 -0600

    added logic to cause program to terminate when gamepad not found

--- logitech/python/detect_gamepad_axis_events.py
+++ logitech/python/detect_gamepad_axis_events.py
@@ -1,40 +1,50 @@
-from evdev import InputDevice, categorize, ecodes, KeyEvent, util
 
-devices = [InputDevice(fn) for fn in util.list_devices()]
+def run():
+	from evdev import InputDevice, categorize, ecodes, KeyEvent, util
+
+	devices = [InputDevice(fn) for fn in util.list_devices()]
+
+	found = False
+
+	for device in devices:
+
+		if device.name == 'Logitech Gamepad F710' or device.name == 'Logitech Gamepad F310':
+			gamePad = InputDevice(device.fn)
+			found = True
+			break
+
+	if found == False:
+		print("Logitech Gamepad device not found")
+		return 
+
+	print("\nPress thumbstick\n")
 
 
-for device in devices:
+	for event in gamePad.read_loop():
+		# event type 3 is EV_ABS
+		if event.type == ecodes.EV_ABS:
+			absaxisevent = categorize(event)
+			print(absaxisevent)
+			print("")
 
-	if device.name == 'Logitech Gamepad F710' or device.name == 'Logitech Gamepad F310':
-		gamePad = InputDevice(device.fn)
-		break
-
-
-print("\nPress thumbstick\n")
-
-
-for event in gamePad.read_loop():
-	# event type 3 is EV_ABS
-	if event.type == ecodes.EV_ABS:
-		absaxisevent = categorize(event)
-		print(absaxisevent)
-		print("")
-
-		if event.code == ecodes.ABS_X:   # ecodes.ABS_X is 3
-			print('ABS_X detected, event.value: ', event.value)
-		elif event.code == ecodes.ABS_Y: # ecodes.ABS_Y is 1
-			print('ABS_Y detected, event value: ', event.value)
-		elif event.code == ecodes.ABS_RX:
-			print('ABS_RX dectected, event.value: ', event.value)
-		elif event.code == ecodes.ABS_RY:
-			print('ABS_RY detected, event.value: ', event.value)
-		elif event.code == ecodes.ABS_Z:
-			print('ABS_Z detected, event.value: ', event.value)
-		elif event.code == ecodes.ABS_RZ:
-			print('ABS_RZ detected, event.value: ', event.value)
-		elif event.code == ecodes.ABS_HAT0X:
-			print('ABS_HAT0X detected, event.value: ', event.value)
-		elif event.code == ecodes.ABS_HAT0Y:
-			print('ABS_HAT0Y detected, event.value: ', event.value)
+			if event.code == ecodes.ABS_X:   # ecodes.ABS_X is 3
+				print('ABS_X detected, event.value: ', event.value)
+			elif event.code == ecodes.ABS_Y: # ecodes.ABS_Y is 1
+				print('ABS_Y detected, event value: ', event.value)
+			elif event.code == ecodes.ABS_RX:
+				print('ABS_RX dectected, event.value: ', event.value)
+			elif event.code == ecodes.ABS_RY:
+				print('ABS_RY detected, event.value: ', event.value)
+			elif event.code == ecodes.ABS_Z:
+				print('ABS_Z detected, event.value: ', event.value)
+			elif event.code == ecodes.ABS_RZ:
+				print('ABS_RZ detected, event.value: ', event.value)
+			elif event.code == ecodes.ABS_HAT0X:
+				print('ABS_HAT0X detected, event.value: ', event.value)
+			elif event.code == ecodes.ABS_HAT0Y:
+				print('ABS_HAT0Y detected, event.value: ', event.value)
 
 		
+
+if __name__ == "__main__":
+	run()
