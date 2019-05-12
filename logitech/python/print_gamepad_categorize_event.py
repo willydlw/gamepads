52628fa810d4db4610ea9fb16a96d06f5391e81f
willydlw <willydlw@comcast.net>
2019-05-12 09:21:03 -0600

    added logic to only print when device is found, else print device not found

--- logitech/python/print_gamepad_categorize_event.py
+++ logitech/python/print_gamepad_categorize_event.py
@@ -1,16 +1,21 @@
+''' prints event category information '''
 import evdev 
 
 devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
 
+found = False 
 
 for device in devices:
 
 	if device.name == 'Logitech Gamepad F710' or device.name == 'Logitech Gamepad F310':
 		gamePad = evdev.InputDevice(device.fn)
+		found = True
 		break
 
+if found:
+	print("\nPrinting gamepad event categories")
 
-print("\nPrinting gamepad event categories")
-
-for event in gamePad.read_loop():
-	print(evdev.categorize(event))
+	for event in gamePad.read_loop():
+		print(evdev.categorize(event))
+else:
+	print("Device not found")
